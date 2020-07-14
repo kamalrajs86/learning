0472da7df0bb784b391f65c4c1c7c4c719c51be1
Kamalraj Selvaraj <kamalrajs_86@hotmail.com>
2020-07-14 17:02:16 -0400

    Docker basic

---
+++ docker/docker_py_test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import snowflake.connector
+
+n = 5
+k = n + n
+print(k)
+
+yr = open("/dockercon/input.dat","r")
+print(yr.read())
+yr.close()
+
+con = snowflake.connector.connect(
+                user="KAMALRAJS",
+                password="Inautix$1",
+                account="sla35871.us-east-1",
+                warehouse="COMPUTE_WH",
+                database="SFPRO",
+                schema="WIP_SCH",
+                role="SF_PRO_ROLE"
+)
+
+cur = con.cursor()
+cur.execute("select * from SFPRO.WIP_SCH.EMPDETAILS1;")
+rows = cur.fetchall()
+print(rows)
